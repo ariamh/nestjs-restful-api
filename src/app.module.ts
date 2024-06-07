dbf4f14c6113142fce1215dba72c3d5e00745513
aria hidayat <33375615+ariamh@users.noreply.github.com>
2024-06-08 00:50:34 +0700

    add .env :basketball:

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { ItemController } from './items/item.controller';
 import { AppService } from './app.service';
@@ -8,7 +9,16 @@ import { ItemSchema } from './items/item.schema'
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb+srv://ariamustofa:fgBFaKSTcOVDHrCF@cluster0.01qwlka.mongodb.net/'),
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI')
+      }),
+      inject: [ConfigService]
+    }),
     MongooseModule.forFeature([{name: 'Item', schema: ItemSchema}])
   ],
   controllers: [AppController, ItemController],
